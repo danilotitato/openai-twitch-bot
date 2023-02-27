6b1810b95ce78f29933ad7ce85bcfce98fc09417
Danilo Titato <danilotitato@gmail.com>
2023-02-27 11:09:49 -0300

    Add a profanity filter to avoid the use of slurs

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const tmi = require('tmi.js');
 const { Configuration, OpenAIApi } = require("openai");
+const axios = require('axios');
 
 const {
     TWITCH_BOT_USERNAME,
     TWITCH_OAUTH_TOKEN,
     TWITCH_CHANNEL,
     OPENAI_API_KEY,
+    BANNED_WORDS_URL
 } = process.env;
 
 const twitchOpts = {
@@ -56,8 +58,16 @@ client.on('message', async (channel, tags, message, self) => {
     // Just answer if it mentions the bot
     if (!message.toLowerCase().includes(`@${botUsername}`)) { return; }
 
+    const bannedWords = await getBannedWords();
+
     // Extract the prompt
     const prompt = message.replace(new RegExp(`@${botUsername}\\s*`), '');
+
+    for (let word of bannedWords) {
+        if (prompt.toLowerCase().includes(word.toLowerCase())) {
+            throw new Error(`Sorry, I cannot provide a response to that prompt. Please try something else.`);
+        }
+    }
 
     try {
         const completion = await openai.createCompletion({
@@ -65,21 +75,37 @@ client.on('message', async (channel, tags, message, self) => {
             prompt
         })
 
-        const response = completion.data.choices[0].text.trim();
+        // Add user mention to the response
+        const response = `@${tags.username}, ${completion.data.choices[0].text.trim()}`;
 
-        // Add user mention
-        const mentionedResponse = `@${tags.username}, ${response}`;
+        for (let word of bannedWords) {
+            if (response.toLowerCase().includes(word.toLowerCase())) {
+                throw new Error(`Sorry, I cannot provide a response to that prompt. Please try something else.`);
+            }
+        }
 
         // Limit the response length
-        const limitedResponse = mentionedResponse.slice(0, maxResponseLength);
+        if (response.length > maxResponseLength) {
+            throw new Error('Sorry, I cannot provide a response that long. Please try a shorter prompt.');
+        }
 
         // Send the response back to the chat
-        client.say(channel, limitedResponse);
+        client.say(channel, response);
     } catch (err) {
         console.error(err);
-        client.say(channel, 'Sorry, an error occurred');
+        client.say(channel, `@${tags.username}, ${err}`);
     }
 });
 
 // Connect to Twitch
 client.connect();
+
+const getBannedWords = async () => {
+    try {
+        const response = await axios.get(BANNED_WORDS_URL);
+        const bannedWords = response.data.split('\n');
+        return bannedWords;
+    } catch (err) {
+        throw new Error(`Can't load word filter`);
+    }
+}
